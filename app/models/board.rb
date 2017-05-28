e79c5d2c3c35fcdfc6e681392646c3e1dc8bc7b1
gsduong <nguyenduong.ict.hust@gmail.com>
2017-05-28 12:11:05 +0700

    Refactor boards_controller. Use namespace for boards_controller

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,12 +1,17 @@
 class Board < ApplicationRecord
+
+  scope :by_ids, -> (ids) {where(id: ids)}
+
   # Define has_many User through board_members
-  has_many  :board_members, :class_name => 'BoardMember'
+  has_many  :board_members, :class_name => 'BoardMember', dependent: :destroy
   has_many  :users, :through => :board_members
 
   # Define has_many List
-  has_many  :lists
+  has_many  :lists, dependent: :destroy
 
   # Define has_many BoardActivity
   has_many  :board_activities
+
+  validates :name, :presence => true
 
 end
--- app/models/board_member.rb
+++ app/models/board_member.rb
@@ -1,5 +1,5 @@
 class BoardMember < ApplicationRecord
-  belongs_to  :user, :class_name => 'User'
+  belongs_to  :user, :class_name => 'User', :foreign_key => 'member_id'
   belongs_to  :board
 
   has_many :lists
