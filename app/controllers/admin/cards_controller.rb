dede7c91e7e108c44a68b97df99f6fbec218002d
gsduong <nguyenduong.ict.hust@gmail.com>
2017-06-01 08:45:24 +0700

    Review phase finished. Refactor

--- app/controllers/admin/cards_controller.rb
+++ app/controllers/admin/cards_controller.rb
@@ -2,15 +2,13 @@ class Admin::CardsController < ApplicationController
 
   before_action :set_board
 
-  before_action :set_card, :only => [:edit, :update, :destroy]
-
-  before_action :require_admin, :only => [:new, :create, :edit, :update, :destroy]
-
   def new
     @card = Card.new
   end
 
   def create
+
+    redirect_to admin_board_path(@board) unless (current_user.is_admin?(@board))
 
     @card = Card.new(params.require(:card).permit(:title, :list_id, :due_date, :description, :progress))
     if @card.save
@@ -24,9 +22,17 @@ class Admin::CardsController < ApplicationController
   end
 
   def edit
+    @card = Card.find(params[:id])
   end
 
   def update
+
+    @card = Card.find(params[:id])
+
+    redirect_to admin_board_path(@board) unless (!(@card.nil?))
+
+    redirect_to admin_board_path(@board) unless ((@card.list.board_id == @board.id) && (current_user.is_admin?(@board)))
+
     if @card.update(params.require(:card).permit(:title, :list_id, :due_date, :description, :progress))
       flash[:success] = 'Successfully updated card!'
       redirect_to admin_board_path(@board)
@@ -34,6 +40,8 @@ class Admin::CardsController < ApplicationController
   end
 
   def destroy
+    @card = Card.find(params[:id])
+    redirect_to admin_board_path(@board) unless ((@card.list.board_id == @board.id) && (current_user.is_admin?(@board)))
     @card.destroy
     flash[:success] = 'Successfully deleted card!'
     redirect_to admin_board_path(@board)
@@ -43,18 +51,5 @@ class Admin::CardsController < ApplicationController
     def set_board
       @board = Board.find(params[:board_id])
       redirect_to boards_path unless (!@board.nil?)
-    end
-
-    def set_card
-      @card = Card.find(params[:id])
-      redirect_to admin_board_path(@board) unless (!@card.nil?)
-    end
-
-    def require_admin
-      if (@card = Card.find_by(params[:id]))
-        redirect_to admin_board_path(@board) unless ((@card.list.board_id == @board.id) && (current_user.is_admin?(@board)))
-      else
-        redirect_to admin_board_path(@board) unless (current_user.is_admin?(@board))
-      end
     end
 end
---
+++ app/controllers/card_members_controller.rb
@@ -0,0 +1,2 @@
+class CardMembersController < ApplicationController
+end
